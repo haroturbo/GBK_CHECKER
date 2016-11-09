18712d47d8f1c44106882b00dbade733527ec5e8
haroturbo <imasperfume@gmail.com>
2016-11-10 07:28:04 +0900

    フォントぱっち

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,7 +58,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(388, 12);
+            this.button2.Location = new System.Drawing.Point(407, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -78,24 +79,35 @@
             // 
             this.button3.Location = new System.Drawing.Point(284, 12);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(117, 23);
             this.button3.TabIndex = 4;
-            this.button3.Text = "フォント作成";
+            this.button3.Text = "EUC-JPフォント作成";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(499, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "ぱっち";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(586, 273);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GBK_CHEACKER";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -109,6 +121,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
@@ -12,8 +13,12 @@ using System.Drawing.Imaging;
 
 namespace WindowsFormsApplication3
 {
+
+
     public partial class Form1 : Form
     {
+        string last_font = "";
+
         public Form1()
         {
             InitializeComponent();
@@ -34,7 +39,7 @@ namespace WindowsFormsApplication3
             StringBuilder sb = new StringBuilder();
 
             byte[] cp = new byte[2];
-            for(int i = 0; i < bs.Length; i += 2)
+            for (int i = 0; i < bs.Length; i += 2)
             {
                 cp[0] = bs[i];
                 cp[1] = bs[i + 1];
@@ -76,39 +81,41 @@ namespace WindowsFormsApplication3
             int len = 0;
             int a = 0;
             int b = 0;
-            int max = 32*10000;
+            int max = 32 * 10000;
 
-            if (max > bs.Length) {
+            if (max > bs.Length)
+            {
 
                 max = bs.Length;
             }
 
             //200x100サイズのImageオブジェクトを作成する
-            Bitmap img = new Bitmap(256,  max/32*2);
+            Bitmap img = new Bitmap(256, max / 32 * 2);
             int k = 0;
-            for (int i = 0; i < max; i=i+2)
+            for (int i = 0; i < max; i = i + 2)
             {
                 f = bs[i];
-                f2 = bs[i+1];
+                f2 = bs[i + 1];
                 len = i >> 1;
-                a = ((len/16)*16)%256;
-                b = (k % 16)+(len / 256)*16;
+                a = ((len / 16) * 16) % 256;
+                b = (k % 16) + (len / 256) * 16;
                 k++;
                 if (k == 16) { k = 0; }
-                if ((f & 1)!=0){ 
-                img.SetPixel(7 + a, b, c);
+                if ((f & 1) != 0)
+                {
+                    img.SetPixel(7 + a, b, c);
                 }
                 if ((f & 2) != 0)
                 {
-                    img.SetPixel(6 + a,  b, c);
+                    img.SetPixel(6 + a, b, c);
                 }
                 if ((f & 4) != 0)
                 {
-                    img.SetPixel(5 + a,  b, c);
+                    img.SetPixel(5 + a, b, c);
                 }
                 if ((f & 8) != 0)
                 {
-                    img.SetPixel(4 + a,   b, c);
+                    img.SetPixel(4 + a, b, c);
                 }
                 if ((f & 16) != 0)
                 {
@@ -116,50 +123,50 @@ namespace WindowsFormsApplication3
                 }
                 if ((f & 32) != 0)
                 {
-                    img.SetPixel(2 + a,   b, c);
+                    img.SetPixel(2 + a, b, c);
                 }
                 if ((f & 64) != 0)
                 {
-                    img.SetPixel(1 + a,   b, c);
+                    img.SetPixel(1 + a, b, c);
                 }
                 if ((f & 128) != 0)
                 {
-                    img.SetPixel(0 + a,   b, c);
+                    img.SetPixel(0 + a, b, c);
                 }
 
 
 
                 if ((f2 & 1) != 0)
                 {
-                    img.SetPixel(15 + a,   b, c);
+                    img.SetPixel(15 + a, b, c);
                 }
                 if ((f2 & 2) != 0)
                 {
-                    img.SetPixel(14 + a,  b, c);
+                    img.SetPixel(14 + a, b, c);
                 }
                 if ((f2 & 4) != 0)
                 {
-                    img.SetPixel( 13 + a,  b, c);
+                    img.SetPixel(13 + a, b, c);
                 }
                 if ((f2 & 8) != 0)
                 {
-                    img.SetPixel( 12 + a,  b, c);
+                    img.SetPixel(12 + a, b, c);
                 }
                 if ((f2 & 16) != 0)
                 {
-                    img.SetPixel( 11 + a,  b, c);
+                    img.SetPixel(11 + a, b, c);
                 }
                 if ((f2 & 32) != 0)
                 {
-                    img.SetPixel( 10 + a,  b, c);
+                    img.SetPixel(10 + a, b, c);
                 }
                 if ((f2 & 64) != 0)
                 {
-                    img.SetPixel( 9 + a,  b, c);
+                    img.SetPixel(9 + a, b, c);
                 }
                 if ((f2 & 128) != 0)
                 {
-                    img.SetPixel( 8 + a,  b, c);
+                    img.SetPixel(8 + a, b, c);
                 }
 
             }
@@ -172,7 +179,7 @@ namespace WindowsFormsApplication3
         private void button3_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-             ofd.Filter = "FONTX2ファイル(*.FNT;*.TLF;*.BIN)|*.FNT;*.TLF;*.BIN|すべてのファイル(*.*)|*.*";
+            ofd.Filter = "FONTX2ファイル(*.FNT;*.TLF;)|*.FNT;*.TLF|すべてのファイル(*.*)|*.*";
             ofd.Title = "開くファイルを選択してください";
             ofd.CheckFileExists = true;
             ofd.CheckPathExists = true;
@@ -183,25 +190,45 @@ namespace WindowsFormsApplication3
 
 
 
-                System.IO.FileStream fs = new System.IO.FileStream( ofd.FileName, System.IO.FileMode.Open,System.IO.FileAccess.Read);
+                System.IO.FileStream fs = new System.IO.FileStream(ofd.FileName, System.IO.FileMode.Open, System.IO.FileAccess.Read);
                 byte[] bs = new byte[18];
                 fs.Read(bs, 0, 18);
 
-                string fontx2 = Encoding.GetEncoding(932).GetString(bs).Substring(0,6);
-                if (fontx2.Contains("FONTX2") == false) {
+                string fontx2 = Encoding.GetEncoding(932).GetString(bs).Substring(0, 6);
+                if (fontx2.Contains("FONTX2") == false)
+                {
                     MessageBox.Show("fontx2以外のフォントは変換できません");
                     return;
                 }
-                if (bs[14] != 16 || bs[15] != 16) {
-                    MessageBox.Show("16x16以外のフォントは変換できません");
+                if ((bs[14] != 8 || bs[14] != 16) && bs[15] != 16)
+                {
+                    MessageBox.Show("8x16,16x16以外のフォントは変換できません");
                     return;
                 }
-                if (bs[16] != 1) {
+                if (bs[16] > 1)
+                {
 
-                    MessageBox.Show("SHIFT-JIS以外のフォントは変換できません");
+                    MessageBox.Show("ASCII,SHIFT-JIS以外のフォントは変換できません");
                     return;
                 }
 
+                if (bs[14] == 8 && bs[16] == 0)
+                {
+                    byte[] asciis = new byte[1520 * 2];
+                    fs.Read(asciis, 0, 1520 * 2);
+
+                    System.IO.FileStream afs = new System.IO.FileStream("ASCII.bin", System.IO.FileMode.Create, System.IO.FileAccess.Write);
+                    //バイト型配列の内容をすべて書き込む
+                    afs.Write(asciis, 32 * 16 - 1, 1520);
+                    //閉じる
+                    afs.Close();
+                    fs.Close();
+                    MessageBox.Show("半角フォントASCII.binが作成されました");
+
+                    //3C 42 A5 81 A5 99 42 3C から　- 0x610,
+
+                    return;
+                }
 
                 int encode = bs[16];
                 int tablelen = bs[17];
@@ -248,9 +275,9 @@ namespace WindowsFormsApplication3
                         Array.ConstrainedCopy(font, i * 32, gohan, pos, 32);
                     }
                 }
-                byte[] n = new byte[8];              
+                byte[] n = new byte[8];
 
-                Array.Resize(ref gohan, 268736 - 0x5f0-8);//267,216 ばいと
+                Array.Resize(ref gohan, 268736 - 0x5f0 - 8-8);//267,208 ばいと
 
 
 
@@ -260,11 +287,86 @@ namespace WindowsFormsApplication3
                 ffs.Write(n, 0, 8);
                 //閉じる
                 ffs.Close();
-                
+                MessageBox.Show("全角EUC-JPフォント FS.bin が作成されました、 bmpみるでプレビューできます");
+                last_font = Encoding.GetEncoding(932).GetString(bs).Substring(6, 8).Trim();
 
                 return;
             }
 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "suprxファイル(*.suprx)|*.suprx|すべてのファイル(*.*)|*.*";
+            ofd.Title = "開くファイルを選択してください";
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                
+                System.IO.FileStream fs = new System.IO.FileStream(ofd.FileName, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                byte[] bs = new byte[fs.Length];
+                fs.Read(bs, 0, bs.Length);
+                fs.Close();
+                int patchpos = 0;
+                for (int i = 0; i < bs.Length; i++) {
+                    if (bs[i] == 0x3C) {
+                        //3C 42 A5 81 A5 99 42 3C から　- 0x610,
+                        if(bs[i+1]==0x42 && bs[i + 2] == 0xa5 && bs[i + 3] == 0x81 && bs[i + 4] == 0xa5 && bs[i + 5] == 0x99 && bs[i + 6] == 0x42 && bs[i + 7] == 0x3c)
+                        {
+                            patchpos = i;
+                            break;
+
+
+                        }
+                    }
+                }
+                if (patchpos == 0) {
+                    MessageBox.Show("パッチ箇所が見つかりませんでした");
+                }
+
+               string exe= System.Windows.Forms.Application.StartupPath;
+
+                if (File.Exists(CombinePaths(exe,"ASCII.bin"))==true) {
+
+                    System.IO.FileStream afs = new System.IO.FileStream("ASCII.bin", System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                    byte[] ascii = new byte[afs.Length];
+                    afs.Read(ascii, 0, ascii.Length);
+                    afs.Close();
+                    Array.ConstrainedCopy(ascii, 0, bs, patchpos - 0x610, ascii.Length);
+                }
+
+                if (File.Exists(CombinePaths(exe, "FS.bin"))==true)
+                {
+                    System.IO.FileStream zfs = new System.IO.FileStream("FS.bin", System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                    byte[] zenkaku = new byte[zfs.Length];
+                    zfs.Read(zenkaku, 0, zenkaku.Length);
+                    zfs.Close();
+                    Array.ConstrainedCopy( zenkaku, 0, bs, patchpos - 0x610 - 267216+8, zenkaku.Length);                    
+                }
+
+
+
+                System.IO.FileStream ffs = new System.IO.FileStream(last_font+".suprx", System.IO.FileMode.Create, System.IO.FileAccess.Write);
+                ffs.Write(bs, 0, bs.Length);
+                ffs.Close();
+                MessageBox.Show("全角EUC-JPフォントがぱっちされました");
+
+
+
+            }
+        }
+
+
+        public static string CombinePaths(string path1, string path2)
+        {
+            path1 = path1.TrimEnd(System.IO.Path.DirectorySeparatorChar);
+            path2 = path2.TrimStart(System.IO.Path.DirectorySeparatorChar);
+            return path1 + System.IO.Path.DirectorySeparatorChar + path2;
         }
     }
 }
